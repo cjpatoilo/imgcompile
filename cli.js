211275bad8557d98db8e2429dd8644a0c447bbd0
CJ Patoilo <cjpatoilo@gmail.com>
2020-08-01 04:49:29 -0300

    Move files to root

--- cli.js
+++ cli.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 'use strict'
-const imgcompile = require('../')
-const pkg = require('../package.json')
+const imgcompile = require('.')
+const pkg = require('./package.json')
 const argv = process.argv.slice(2)
 
 if (argv.indexOf('--version') !== -1 || argv.indexOf('-v') !== -1) {
-	console.log(pkg.version)
-	process.exit(0)
+  console.log(pkg.version)
+  process.exit(0)
 }
 
 if (argv.indexOf('--help') !== -1 || argv.indexOf('-h') !== -1) {
-	console.log(`
+  console.log(`
   Usage:
 
     $ imgcompile <input> [<output>] [<options>]
@@ -24,8 +24,8 @@ if (argv.indexOf('--help') !== -1 || argv.indexOf('-h') !== -1) {
 
     $ imgcompile src
     $ imgcompile src dist
-	`)
-	process.exit(0)
+`)
+  process.exit(0)
 }
 
 imgcompile(argv[0], argv[1])
